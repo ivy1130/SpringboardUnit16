b767a1a63f53b212c83c47ca9d0190bf640d4835
Ivy <ivy@onprem.com>
2022-02-21 23:23:10 -0600

    completed part 2, which is creating new stories

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 
 /******************************************************************************
  * Handling navbar clicks and updating navbar
@@ -6,31 +6,41 @@
 
 /** Show main list of all stories when click site name */
 
-function navAllStories(evt) {
-  console.debug("navAllStories", evt);
-  hidePageComponents();
-  putStoriesOnPage();
+function navAllStories (evt) {
+	console.debug('navAllStories', evt)
+	hidePageComponents()
+	putStoriesOnPage()
 }
 
-$body.on("click", "#nav-all", navAllStories);
+$body.on('click', '#nav-all', navAllStories)
 
 /** Show login/signup on click on "login" */
 
-function navLoginClick(evt) {
-  console.debug("navLoginClick", evt);
-  hidePageComponents();
-  $loginForm.show();
-  $signupForm.show();
+function navLoginClick (evt) {
+	console.debug('navLoginClick', evt)
+	hidePageComponents()
+	$loginForm.show()
+	$signupForm.show()
 }
 
-$navLogin.on("click", navLoginClick);
+$navLogin.on('click', navLoginClick)
 
 /** When a user first logins in, update the navbar to reflect that. */
 
-function updateNavOnLogin() {
-  console.debug("updateNavOnLogin");
-  $(".main-nav-links").show();
-  $navLogin.hide();
-  $navLogOut.show();
-  $navUserProfile.text(`${currentUser.username}`).show();
+function updateNavOnLogin () {
+	console.debug('updateNavOnLogin')
+	$('.main-nav-links').show()
+	$navLogin.hide()
+	$navLogOut.show()
+	$navSubmit.show()
+	$navUserProfile.text(`${currentUser.username}`).show()
 }
+
+// Show submit form on click on "submit"
+
+function navSubmitClick (evt) {
+	console.debug('navSubmitClick', evt)
+	$submitStoryForm.show()
+}
+
+$navSubmit.on('click', navSubmitClick)
--- js/stories.js
+++ js/stories.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict'
 
 // This is the global list of the stories, an instance of StoryList
-let storyList;
+let storyList
 
 /** Get and show stories when site first loads. */
 
-async function getAndShowStoriesOnStart() {
-  storyList = await StoryList.getStories();
-  $storiesLoadingMsg.remove();
+async function getAndShowStoriesOnStart () {
+	storyList = await StoryList.getStories()
+	$storiesLoadingMsg.remove()
 
-  putStoriesOnPage();
+	putStoriesOnPage()
 }
 
 /**
@@ -19,11 +19,11 @@ async function getAndShowStoriesOnStart() {
  * Returns the markup for the story.
  */
 
-function generateStoryMarkup(story) {
-  // console.debug("generateStoryMarkup", story);
+function generateStoryMarkup (story) {
+	// console.debug("generateStoryMarkup", story);
 
-  const hostName = story.getHostName();
-  return $(`
+	const hostName = story.getHostName()
+	return $(`
       <li id="${story.storyId}">
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
@@ -32,21 +32,52 @@ function generateStoryMarkup(story) {
         <small class="story-author">by ${story.author}</small>
         <small class="story-user">posted by ${story.username}</small>
       </li>
-    `);
+    `)
 }
 
 /** Gets list of stories from server, generates their HTML, and puts on page. */
 
-function putStoriesOnPage() {
-  console.debug("putStoriesOnPage");
+function putStoriesOnPage () {
+	console.debug('putStoriesOnPage')
 
-  $allStoriesList.empty();
+	$allStoriesList.empty()
 
-  // loop through all of our stories and generate HTML for them
-  for (let story of storyList.stories) {
-    const $story = generateStoryMarkup(story);
-    $allStoriesList.append($story);
-  }
+	// loop through all of our stories and generate HTML for them
+	for (let story of storyList.stories) {
+		const $story = generateStoryMarkup(story)
+		$allStoriesList.append($story)
+	}
 
-  $allStoriesList.show();
+	$allStoriesList.show()
 }
+
+// Get submit form inputs, add it to the server, generate its HTML, and put it on the page
+
+async function putNewStoryOnPage (evt) {
+	console.debug('putNewStoryOnPage', evt)
+	evt.preventDefault()
+
+	const author = $('#submit-story-author').val()
+	const title = $('#submit-story-title').val()
+	const url = $('#submit-story-url').val()
+
+	const newStory = await storyList.addStory(currentUser, {author, title, url})
+
+	const $newStory = generateStoryMarkup(newStory)
+
+	$allStoriesList.prepend($newStory)
+	$submitStoryForm.trigger('reset')
+	$submitStoryForm.hide()
+}
+
+$submitStoryForm.on('submit', putNewStoryOnPage)
+
+// Clear submit form inputs and hide form when user clicks "cancel"
+
+function cancelSubmitStory (evt) {
+	evt.preventDefault()
+	$submitStoryForm.trigger('reset')
+	$submitStoryForm.hide()
+}
+
+$('#cancel-submit-story-button').on('click', cancelSubmitStory)
